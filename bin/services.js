2de1c184ba2b401acac247a0ae35bfcbf2a262ab
ssunagar <ssunagar@deloitte.com>
2016-05-21 00:19:29 +0530

    Added contentful-manager related changes.
    Added changes and the same are mentioned here,
    1 Added contentType parameter.
    2 Renamed service from deleteEntries to removeEntries as there were two
    methods with the same name.
    3 Added changes related to deleteEntries.
    4 Added changes to index js file to refer request params.

--- bin/services.js
+++ bin/services.js
@@ -2,40 +2,40 @@ const _ = require('lodash');
 const RateLimiter = require('limiter').RateLimiter;
 const contentful = require('contentful-management');
 const ACCESS_TOKEN = '';
-const SPACE_ID = '';;
+const SPACE_ID = '';
 const client = contentful.createClient({
-  accessToken: ACCESS_TOKEN,
-  secure: true
+    accessToken: ACCESS_TOKEN,
+    secure: true
 });
 
 module.exports = {
-  getContentTypes: getContentTypes,
-  deleteEntries: deleteEntries
+    getContentTypes: getContentTypes,
+    removeEntries: removeEntries
 }
 
-function deleteEntries(contentTypeId, callback) {
-  getEntries(contentTypeId, true, function(err, entries) {
-    if(err) callback(err, false);
-    var ids = _.map(entries, 'sys.id');
+function removeEntries(contentTypeId, callback) {
+    getEntries(contentTypeId, true, function(err, entries) {
+        if (err) callback(err, false);
+        //var ids = _.map(entries, 'sys.id');
 
-    deleteEntries(entries, function(err, result) {
-      callback(err, result);
+        deleteEntries(entries, function(err, result) {
+            callback(err, result);
+        });
     });
-  });
 }
 
 function deleteEntries(entries, callback) {
-  getSpace(function(err, space) {
-console.log('del');
-    for(var i = 0; i < entries.length; i++) {
-      space.deleteEntry(entry).catch(function(err) {
-        console.log(err);
-      }).then(function(result) {
-        console.log(result);
-      });
-    }
-    //callback(null, true);
-  });
+    getSpace(function(err, space) {
+        console.log('del');
+        for (var i = 0; i < entries.length; i++) {
+            space.deleteEntry(entries[i]).catch(function(err) {
+                console.log(err);
+            }).then(function(result) {
+                console.log(result);
+            });
+        }
+        callback(null, true);
+    });
 }
 
 // function deleteEntriesBulk(entries, callback) {
@@ -54,29 +54,31 @@ console.log('del');
 // }
 
 function getEntries(contentType, isPublished, callback) {
-  getSpace(function(err, space) {
-    space.getEntries({content_type: contentType}).catch(function(err) {
-      callback(err, false);
-    }).then(function(entries) {
-      callback(false, entries)
+    getSpace(function(err, space) {
+        space.getEntries({
+            content_type: contentType
+        }).catch(function(err) {
+            callback(err, false);
+        }).then(function(entries) {
+            callback(false, entries)
+        });
     });
-  });
 }
 
 function getContentTypes(callback) {
-  getSpace(function(err, space) {
-    space.getContentTypes().catch(function(err) {
-      callback(err, false);
-    }).then(function(contentTypes) {
-      callback(false, contentTypes);
-    });
-  })
+    getSpace(function(err, space) {
+        space.getContentTypes().catch(function(err) {
+            callback(err, false);
+        }).then(function(contentTypes) {
+            callback(false, contentTypes);
+        });
+    })
 }
 
 function getSpace(callback) {
-  client.getSpace(SPACE_ID).catch(function(err) {
-    callback('Could not find space: ' + err, false);
-  }).then(function(space) {
-    callback(false, space);
-  });
+    client.getSpace(SPACE_ID).catch(function(err) {
+        callback('Could not find space: ' + err, false);
+    }).then(function(space) {
+        callback(false, space);
+    });
 }
